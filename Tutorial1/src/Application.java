ae0455b0d38e26e8568df8163c21040c7312d425
afournierca <afournierca@gmail.com>
2024-05-08 10:51:44 -0400

    More variables

--- Tutorial1/src/Application.java
+++ Tutorial1/src/Application.java
@@ -1,11 +1,31 @@
 public class Application {
 
 	public static void main(String[] args) {
-		int myNumber;
-		myNumber = 42;
+		int myNumber = 42;
+		short myShort = 847;
+		long myLong = 9797;
+		double myDouble = 7.3243;
+		float myFloat = 324.3f;
+		
+		char myChar = 'y';
+		boolean myBoolean = false;
+		byte myByte = 127;
 		
 		System.out.println("Meaning of Life the Universe and everything:");
 		System.out.println(myNumber);
-		
+		System.out.println("Short");
+		System.out.println(myShort);
+		System.out.println("Long");
+		System.out.println(myLong);
+		System.out.println("Double");
+		System.out.println(myDouble);
+		System.out.println("Float");
+		System.out.println(myFloat);
+		System.out.println("Char");
+		System.out.println(myChar);
+		System.out.println("Boolean");
+		System.out.println(myBoolean);
+		System.out.println("Byte");
+		System.out.println(myByte);
 	}
 }
