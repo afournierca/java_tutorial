0afde4cfa1bc235b7a6437c522393ec1e11d787a
afournierca <afournierca@gmail.com>
2024-05-08 13:08:49 -0400

    While Loops... easy

---
+++ Tutorial4/src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		int value = 0;
+		
+		while(value < 10)
+		{
+			System.out.println("Iterate 10 times: " + value);
+			value = value + 1;
+		}
+	}
+
+}
