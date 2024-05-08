0ac5f53b170907245ac180ed99dc43d4b54ba45d
afournierca <afournierca@gmail.com>
2024-05-08 13:19:08 -0400

    Simple for loop

---
+++ Tutorial5/src/Application.java
@@ -0,0 +1,17 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		// for needs two ;; in loop
+		for (int i = 0; i < 10; i++) {
+
+			System.out.printf("Current iteration: %d\n", i);
+			
+			// shortcut sysout   CTRL Space
+			// System.out.println();
+		}
+
+	}
+
+}
