70348144e107b33157c3a0056669c84274ea6b0a
afournierca <afournierca@gmail.com>
2024-05-08 12:59:35 -0400

    Hitchhiker's Guide to the Galaxy

---
+++ Tutorial3/src/Application.java
@@ -0,0 +1,17 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 42;
+		
+		String myText = "The meaning of life, the univers and everyting :------->";
+		String blank = " ";
+		String name = "Deep Thought";
+		String greeting = myText + blank + name;
+		System.out.println(greeting);
+		System.out.println(myInt);
+		
+		
+		
+		
+	}
+}
