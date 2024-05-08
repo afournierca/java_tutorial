b584be4a5842a168c018607a7ea977ecc4010bf9
afournierca <afournierca@gmail.com>
2024-05-08 10:38:58 -0400

    Starting variables

--- Tutorial1/src/Application.java
+++ Tutorial1/src/Application.java
@@ -1,6 +1,11 @@
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		int myNumber;
+		myNumber = 42;
+		
+		System.out.println("Meaning of Life the Universe and everything:");
+		System.out.println(myNumber);
+		
 	}
 }
