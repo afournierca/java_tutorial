51a59f8dae581a579fd2b09b38b9d299eceb3770
afournierca <afournierca@gmail.com>
2024-05-15 10:15:26 -0400

    first array tutorial

---
+++ Tutorial11/src/Application.java
@@ -0,0 +1,39 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		String[] words = new String[3];
+		
+		words[0] = "Hello";
+		words[1] = "to";
+		words[2] = "you";
+		
+		System.out.println(words[2]);
+		
+		String[] fruits = {"apple","banana","pear","kwi"};
+		
+		for(String fruit: fruits) {
+			System.out.println(fruit);
+		}
+		
+		// starts with lower case --> primitive
+		int value = 0;
+		
+		// starts with upper case --> Class
+		String text = null;
+		
+		System.out.println(text);
+		
+		String[] texts = new String[2];
+
+		System.out.println(texts[0]);
+		
+		texts[0] = "one";
+	
+				
+				
+		
+	}
+
+}
