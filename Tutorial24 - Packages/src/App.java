9585dbdf7e893f016320754c9c3ecb7e94cb8487
afournierca <afournierca@gmail.com>
2024-05-27 13:10:51 -0400

    Package hierarchies and naming convention if you are going to distribute package globally.

---
+++ Tutorial24 - Packages/src/App.java
@@ -0,0 +1,14 @@
+import ocean.Fish;
+import ocean.plants.Seaweed;
+
+
+// Packages are all lower case with no spaces, underscores etc.
+public class App {
+
+	public static void main(String[] args) {
+		
+		Fish fish = new Fish();
+		Seaweed weed = new Seaweed();
+	}
+
+}
---
+++ Tutorial24 - Packages/src/ca/majorpain/oceangame/Aquarium.java
@@ -0,0 +1,7 @@
+package ca.majorpain.oceangame;
+
+
+// example of would wide package.  Using domain in reverse order then package
+public class Aquarium {
+
+}
